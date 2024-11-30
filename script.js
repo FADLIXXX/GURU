7a07676943729e01de2c6658da5f1ccf99b1e044
FADLIXXX <tribudimanandika2@gmail.com>
2024-11-30 17:52:24 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('processBtn').addEventListener('click', () => {
-  const inputText = document.getElementById('inputText').value;
-  
-  // Simulasi logika AI sederhana
-  const output = `AI memproses: "${inputText.toUpperCase()}"`;
-  
-  document.getElementById('outputText').innerText = output;
-});
